0f5979bae634dc1b8b1f8f16c1f14d6d444596ee
sherif <sherifp9@gmail.com>
2026-05-13 20:11:08 +0100

    Added multiple attempts to guessing game

--- guess_game.py
+++ guess_game.py
@@ -1,9 +1,11 @@
 import random
 secret_number = random.randint(1, 10)
-guess = int(input("Guess a number between 1 and 10:"))
-if guess == secret_number:
-    print("Correct! You guessed the number.")
-elif guess < secret_number:
-    print("Too low!")
-else:
-    print("Too high!")
+while True:
+    guess = int(input("Guess a number between 1 and 10:"))
+    if guess == secret_number:
+        print("Correct! You guessed the number.")
+        break
+    elif guess < secret_number:
+        print("Too low!")
+    else:
+        print("Too high!")
