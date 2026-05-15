15c9a469de263444fd788e315310acaddfd75b2c
sherif <sherifp9@gmail.com>
2026-05-15 17:24:16 +0100

    Upgraded calculator with repeat loop

--- calculator.py
+++ calculator.py
@@ -1,15 +1,22 @@
-print("Simple Calculator")
-num1 = float(input("Enter first number: "))
-operation = input("Enter operation (+, -, *, /): ")
-num2 = float(input("Enter second number: "))
-if operation == "+":
-    print("Result:", num1 + num2)
-elif operation == "-":
-    print("Result:", num1 - num2)
-elif operation == "*":
-    print("Result:", num1 * num2)
-elif Operation == "/":
-    print("Result:", num1 / num2)
-else:
-    print("Ivalid operation")
-
+while True:
+    print("Simple Calculator")
+    num1 = float(input("Enter first number: "))
+    operation = input("Enter operation (+, -, *, /): ")
+    num2 = float(input("Enter second number: "))
+    if operation == "+":
+        print("Result:", num1 + num2)
+    elif operation == "-":
+        print("Result:", num1 - num2)
+    elif operation == "*":
+        print("Result:", num1 * num2)
+    elif operation == "/":
+        if num2 == 0:
+            print("Error: Cannot divide by zero!")
+        else:
+            print("Result:", num1 / num2)
+    else:
+        print("Invalid operation")
+    again = input("Do another calculation? (yes/no): ")
+    if again. lower() != "yes":
+        print("Calculator closed.")
+        break
