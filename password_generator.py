1edb8f3e23588cc4d8b0c967f7cb3aa972154202
sherif <sherifp9@gmail.com>
2026-05-15 22:17:45 +0100

    Added password generator

---
+++ password_generator.py
@@ -0,0 +1,7 @@
+import random
+characters = "adcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*"
+length = int(input("Enter password length:"))
+password = ""
+for i in range(length):
+    password += random.choice(characters)
+print("Generated Password:", password)
