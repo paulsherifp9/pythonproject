39659d585a8894beccbba082fdd5802cc34a0790
sherif <sherifp9@gmail.com>
2026-05-18 11:00:30 +0100

    Added Flask portfolio

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
+
+
 @app.route("/")
 def home():
-    return "<h1>Hello, Flask!</h1>"
+    return render_template("index.html")
+
+
 app.run(debug=True)
