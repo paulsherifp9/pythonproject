38710454f56f6f7ed19f8a06a0c4e6fb9e1775a7
sherif <sherifp9@gmail.com>
2026-05-20 15:47:49 +0100

    Prepared portfolio for deployment

--- app.py
+++ app.py
@@ -13,9 +13,43 @@ def projects():
     return render_template("projects.html")
 
 
-@app.route("/contact")
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
-    return render_template("contact.html")
+
+    success = False
+
+    if request.method == "POST":
+
+        name = request.form["name"]
+        email = request.form["email"]
+        message = request.form["message"]
+
+        print("New Message")
+        print("Name:", name)
+        print("Email:", email)
+        print("Message:", message)
+
+        success = True
+
+    return render_template(
+        "contact.html",
+        success=success
+    )
+
 
 if __name__ == "__main__":
     app.run(debug=True)
