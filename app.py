2e711639fe1ab644d855b6efeaf3233312b47875
sherif <sherifp9@gmail.com>
2026-05-24 16:44:52 +0100

    Prepared portfolio for deployment

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, session, flash
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -19,6 +20,17 @@ class ContactMessage(db.Model):
     email = db.Column(db.String(100))
 
     message = db.Column(db.Text)
+
+
+class User(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    username = db.Column(db.String(100), unique=True)
+
+    password = db.Column(db.String(100))
+
+    role = db.Column(db.String(20), default="user")
 
 
 @app.route("/")
@@ -68,6 +80,55 @@ def contact():
         success=success
     )
 
+@app.route("/register", methods=["GET", "POST"])
+def register():
+
+    if request.method == "POST":
+
+        username = request.form["username"]
+
+        password = request.form["password"]
+
+        hashed_password = generate_password_hash(password)
+
+        existing_user = User.query.filter_by(
+            username=username
+        ).first()
+
+        if existing_user:
+
+            flash("Username already exists")
+
+            return redirect("/register")
+
+        if username == "admin":
+
+            user_role = "admin"
+
+        else:
+
+            user_role = "user"
+
+
+        new_user = User(
+
+    username=username,
+
+    password=hashed_password,
+
+    role=user_role
+)
+
+        db.session.add(new_user)
+
+        db.session.commit()
+
+        flash("Registration successful")
+
+        return redirect("/login")
+
+    return render_template("register.html")
+
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -78,7 +139,14 @@ def login():
 
         password = request.form["password"]
 
-        if username == "admin" and password == "1234":
+        user = User.query.filter_by(
+            username=username
+        ).first()
+
+        if user and check_password_hash(
+            user.password,
+            password
+        ):
 
             session["user"] = username
 
@@ -93,9 +161,23 @@ def login():
 @app.route("/messages")
 def messages():
 
-    if "user" not in session:
+    if not session.get("user"):
+
+        flash("Please login first")
 
         return redirect("/login")
+
+
+    user = User.query.filter_by(
+             username=session["user"]
+        ).first()
+
+
+    if user.role != "admin":
+
+            flash("Access denied")
+
+            return redirect("/")
 
     all_messages = ContactMessage.query.all()
 
@@ -105,6 +187,64 @@ def messages():
 
         messages=all_messages
     )
+
+@app.route("/edit_message/<int:id>",
+           methods=["GET", "POST"])
+def edit_message(id):
+
+    message = ContactMessage.query.get(id)
+
+    if request.method == "POST":
+
+        message.name = request.form["name"]
+
+        message.email = request.form["email"]
+
+        message.message = request.form["message"]
+
+        db.session.commit()
+
+        flash("Message updated successfully")
+
+        return redirect("/messages")
+
+    return render_template(
+        "edit_message.html",
+        message=message
+    )
+
+@app.route("/delete_message/<int:id>")
+def delete_message(id):
+
+    user = User.query.filter_by(
+    username=session["user"]
+).first()
+
+    if user.role != "admin":
+
+        flash("Access denied")
+
+        return redirect("/")
+
+    if not session.get("user"):
+
+        flash("Please login first")
+
+        return redirect("/login")
+
+    message = ContactMessage.query.get(id)
+
+    if message:
+
+        db.session.delete(message)
+
+        db.session.commit()
+
+        flash("Message deleted")
+
+    return redirect("/messages")
+
+
 
 @app.route("/logout")
 def logout():
